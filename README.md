5efb84a21e361295f7b7af0f2f94723147f428e5
Rob <gubinrobert@gmail.com>
2019-11-28 17:20:04 +0100

    README fix, Odomontois Cont implementation added.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# continuations_playground
+# Project with lots of code samples on continuations with explaining comments.
+
+This article was hugely inspired by 
+Andrzej Filinski paper "Representing monads"  
+and this post:  
+http://blog.sigfpe.com/2008/12/mother-of-all-monads.html  
+Article path: contarticle/Article.scala  
+It is supposed to be read from start to an end.
--- src/main/scala/contarticle/Article.scala
+++ src/main/scala/contarticle/Article.scala
@@ -680,3 +680,40 @@ object HackageContExamples extends App {
 	assert(whatsYourNameCont("Tom") == "Welcome, Tom!")
 	assert(whatsYourNameCont(null) == "You forgot to tell me your name!")
 }
+
+// Special thanks to https://gist.github.com/Odomontois for providing this nice implementation!
+object ContinuationMonadTrait extends App {
+	import Domain._
+
+	trait Continuation[R, +A] extends ((A => R) => R) {
+		def map[B](f: A => B): Continuation[R, B] =
+			flatMap(a => _(f(a)))
+
+		def flatMap[B](f: A => Continuation[R, B]): Continuation[R, B] =
+			k => this(a => f(a)(k))
+	}
+
+	object Continuation {
+		def pure[R, A](a: A): Continuation[R, A] = _(a)
+
+		def unit[R]: Continuation[R, Unit] = pure(())
+
+		def callCC[R, A, B](f: (A => Continuation[R, B]) => Continuation[R, A]): Continuation[R, A] =
+			k => f(a => _ => k(a))(k)
+	}
+
+	import Continuation._
+	def optional[R, T](v: T, ifNull: => Continuation[R, T]): Continuation[R, T] =
+		callCC[R, T, T](k =>
+			callCC[R, T, T](exit => if (v != null) k(v) else exit(v)).flatMap(_ => ifNull) // ignore because it is always null
+		)
+
+	def programOptional[R, T](id: Long)(ifNull: Continuation[R, Null]): Continuation[R, Info] =
+		optional[R, User](getUser(id), ifNull).flatMap { user =>
+			optional[R, Info](getInfo(user), ifNull)
+		}
+
+	val ifNull: Continuation[String, Null] = _ => "Error: null"
+	assert(programOptional(123)(ifNull).map(_.name)(identity) == "Tom")
+	assert(programOptional(1234)(ifNull).map(_.name)(identity) == "Error: null")
+}
