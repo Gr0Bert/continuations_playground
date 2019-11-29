229277e88331d6c76964d2c20d2c08356bac1a90
Rob <gubinrobert@gmail.com>
2019-11-29 13:30:38 +0100

    minor fix

--- continuations-playground/src/main/scala/contarticle/MonadicEmbedding.scala
+++ continuations-playground/src/main/scala/contarticle/MonadicEmbedding.scala
@@ -19,37 +19,3 @@ object MonadicEmbedding extends App {
 	assert(runM(programOptional[Info](123)) == Optional.pure(Info("Tom")))
 	assert(runM(programOptional[Info](1234)) == Nothing)
 }
-
-object ContinuationCompositionWithSimpleChoice extends App {
-	import Domain._
-
-	case class Continuation[R, +A](run: (A => R) => R) {
-		def map[B](f: A => B): Continuation[R, B] = {
-			this.flatMap((a: A) => Continuation(k => k(f(a))))
-		}
-
-		def flatMap[B](f: A => Continuation[R, B]): Continuation[R, B] = {
-			Continuation(k => run(a => f(a).run(k)))
-		}
-	}
-
-	// Lets explore an opportunity to make a choice - return a different value, say, specific error.
-	// All we need - just a function which return the value of the R (result) type.
-	def optional[R, T](v: T, ifNull: () => R): Continuation[R, T] =
-		Continuation((k: T => R) =>
-			if (v != null) {
-				k(v)
-			} else {
-				ifNull()
-			}
-		)
-
-	def programOptional[R](id: Long)(ifNull: () => R): Continuation[R, Info] =
-		optional(getUser(id), ifNull).flatMap{ user =>
-			optional(getInfo(user), ifNull)
-		}
-
-	val ifNull = () => "Error: null"
-	assert(programOptional(123)(ifNull).map(_.name).run(identity) == "Tom")
-	assert(programOptional(1234)(ifNull).map(_.name).run(identity) == ifNull())
-}
