372be064f4bef788dcd66c11f9c4432804268c69
Rob <gubinrobert@gmail.com>
2019-12-19 16:43:42 +0100

    Error handlers added, bug: doesn't handle async

--- continuations_playground/src/main/scala/concurrency/AsyncIO.scala
+++ continuations_playground/src/main/scala/concurrency/AsyncIO.scala
@@ -6,11 +6,15 @@ object AsyncIO extends App {
 	sealed trait IO[+A] {
 		def flatMap[B](f: A => IO[B]): IO[B] = Cont(this, f)
 		def map[B](f: A => B): IO[B] = flatMap(a => Done(f(a)))
+		def raise(e: Throwable) = Raise(e)
 	}
+	def handle[A](fa: IO[A])(h: Throwable => IO[A]): IO[A] = Handle(fa, h)
 	final case class Done[A](result: A) extends IO[A]
 	final case class More[A](f: () => IO[A]) extends IO[A]
 	final case class Cont[A, B](a: IO[A], f: A => IO[B]) extends IO[B]
-	final case class Async[A](k: (A => Unit) => Unit) extends IO[A]
+	final case class Async[A](k: (Either[Throwable, A] => Unit) => Unit) extends IO[A]
+	final case class Raise[A](e: Throwable) extends IO[A]
+	final case class Handle[A](a: IO[A], h: Throwable => IO[A]) extends IO[A]
 
 	object IO {
 		import scala.concurrent.ExecutionContext.global
@@ -22,32 +26,47 @@ object AsyncIO extends App {
 			}
 		}
 		
-		def async[A](k: (A => Unit) => Unit): IO[A] = Async(k)
+		def async[A](k: (Either[Throwable, A] => Unit) => Unit): IO[A] = Async(k)
 	}
 
-	final def runAsync[A](t: IO[A], cb: A => Unit): Unit = {
+	final def runAsync[A](t: IO[A])(cb: Either[Throwable, A] => Unit): Unit = {
 		t match {
-			case Done(result) => cb(result)
+			case Done(result) => cb(Right(result))
+			case Raise(e) => cb(Left(e))
 			case Async(k) => k(cb)
 			case Cont(Async(k), f) =>
-				val rest = (a: Any) => {
-					runAsync(loop(f(a)), cb)
-					()
+				val rest = (a: Either[Throwable, Any]) => {
+					a match {
+						case Left(value) =>
+							cb(Left(value))
+						case Right(value) =>
+							runAsync[A]{
+								loop(f(value), Nil).asInstanceOf[IO[A]]
+							}(cb)
+							()
+					}
 				}
 				k(rest)
-			case rest => runAsync(loop(rest), cb) 
+			case rest => runAsync[A](loop(rest, Nil).asInstanceOf[IO[A]])(cb) 
 		}
 
 		@scala.annotation.tailrec
-		def loop(t: IO[A]): IO[A] = {
+		def loop(t: IO[Any], handler: List[Throwable => IO[Any]]): IO[Any] = {
+			println(handler.size)
 			t match {
-				case More(k) => loop(k())
-				case Cont(Done(v), f) => loop(f(v))
-				case Cont(More(k), f) => loop(Cont(k(), f))
-				case Cont(Cont(b, g), f) =>
-					loop(
-						Cont(b, (x: Any) => Cont(g(x), f))
-					)
+				case Raise(e) => handler match {
+					case h :: rest => 
+						println("use handler")
+						loop(h(e), rest)
+					case Nil =>
+						println("raise")
+						Raise(e)
+				}
+				case Handle(a, h) => loop(a, h :: handler)
+				case More(k) => loop(k(), handler)
+				case Cont(Done(v), f) => loop(f(v), handler)
+				case Cont(More(k), f) => loop(Cont(k(), f), handler)
+				case Cont(Cont(b, g), f) => loop(Cont(b, (x: Any) => Cont(g(x), f)), handler)
 				case x => x
 			}
 		}
@@ -62,28 +81,33 @@ object AsyncIO extends App {
 	def idTrampolined[A](a: A): IO[A] = Done(a)
 
 	val t =
-		IO.async[Int](k => {
-			Future(() => {
-				println{
-					s"In future thread: ${Thread.currentThread().getName}"
+			IO.async[Int](k => {
+				Future(() => {
+					println{
+						s"In future thread: ${Thread.currentThread().getName}"
+					}
+					k(Right(5))
+				})(IO.ec).map{ x =>
+					x()
+				}(IO.ec)
+			}).flatMap { x =>
+				handle{
+					println {
+					Thread.currentThread().getName
 				}
-				k(5)
-			})(IO.ec).map{ x =>
-				x()
-			}(IO.ec)
-		}).flatMap { x =>
-			println {
-				Thread.currentThread().getName
-			}
-			IO.shift.flatMap{ _ =>
-				println{
-					s"In done thread: ${Thread.currentThread().getName}"
-				}
-				Done(x)
-			}
+				IO.shift.flatMap{ _ =>
+					println{
+						s"In done thread: ${Thread.currentThread().getName}"
+					}
+					Done(x)
+				}.raise(new RuntimeException("test"))
+			}(_ => Done(-1))
 		}
 	println{
-		runAsync(t, (x: Int) => println(x))
-//		runAsync(List.fill(100000)(idTrampolined[Int](_)).foldLeft(idTrampolined[Int](_))(andThenTrampolined)(1), (x: Int) => println(x))
+		runAsync(t)((x: Either[Throwable, Int]) => println(x))
+//		runAsync(
+//			List.fill(100000)(idTrampolined[Int](_)).foldLeft(idTrampolined[Int](_))(andThenTrampolined)(1))(
+//			(x: Either[Throwable, Int]) => println(x)
+//		)
 	}
 }
